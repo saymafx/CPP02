ec880e5a600c7ff682e9a9658398a9c3e7f1cd00
saymafx <93143559+saymafx@users.noreply.github.com>
2023-03-23 21:12:20 +0100

    Add files via upload

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,24 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tidigov <tidigov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/22 14:57:51 by tidigov           #+#    #+#             */
-/*   Updated: 2023/03/23 20:22:39 by tidigov          ###   ########.fr       */
+/*   Created: 2023/03/23 20:01:46 by tidigov           #+#    #+#             */
+/*   Updated: 2023/03/23 20:03:16 by tidigov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main()
-{
-    Fixed a;
-    Fixed b( a );
-    Fixed c;
-
-    c = b;
+int main( void ) {
+    Fixed       a;
+    Fixed const b( 10 );
+    Fixed const c( 42.42f );
+    Fixed const d( b );
     
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
+    a = Fixed( 1234.4321f );
+    
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
     return 0;
-
 }
